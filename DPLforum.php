7263b0f1e9d79fe30eca73219fcdddedc2e70f7c
addshore <addshorewiki@gmail.com>
2014-01-24 21:25:48 +0100

    Fix file comments and add README file
    Change-Id: Ic734c62c7e07c6eae1dde6c6d4a15d51d24a52c4

--- DPLforum.php
+++ DPLforum.php
@@ -1,34 +1,26 @@
 <?php
 /**
-
- DPLforum v3.3.3 -- DynamicPageList-based forum extension
-
- Author: Ross McClure
- http://www.mediawiki.org/wiki/User:Algorithm
-
- DynamicPageList written by: n:en:User:IlyaHaykinson n:en:User:Amgine
- http://en.wikinews.org/wiki/User:Amgine
- http://en.wikinews.org/wiki/User:IlyaHaykinson
-
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License along
- with this program; if not, write to the Free Software Foundation, Inc.,
- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- http://www.gnu.org/copyleft/gpl.html
-
- To install, add following to LocalSettings.php
-   require_once("$IP/extensions/DPLforum/DPLforum.php");
-
-*/
+ * @author Ross McClure http://www.mediawiki.org/wiki/User:Algorithm
+ *
+ * DynamicPageList written by: n:en:User:IlyaHaykinson n:en:User:Amgine
+ * http://en.wikinews.org/wiki/User:Amgine
+ * http://en.wikinews.org/wiki/User:IlyaHaykinson
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ */
 
 if ( !defined( 'MEDIAWIKI' ) ) {
 	echo( "This file is an extension to the MediaWiki software and is not a valid access point" );
---
+++ README.md
@@ -0,0 +1,15 @@
+# DPLforum
+
+DPLforum v3.4.0 -- DynamicPageList-based forum extension
+
+Author: Ross McClure
+http://www.mediawiki.org/wiki/User:Algorithm
+
+DynamicPageList written by:
+ -  n:en:User:Amgine - http://en.wikinews.org/wiki/User:Amgine
+ -  n:en:User:IlyaHaykinson - http://en.wikinews.org/wiki/User:IlyaHaykinson
+
+## Installation
+
+To install, add following to LocalSettings.php
+ > require_once("$IP/extensions/DPLforum/DPLforum.php");
