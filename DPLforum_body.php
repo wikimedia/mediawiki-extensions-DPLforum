5800cb077dd8126187321a671672526b839b92d3
Jack Phoenix <jack@countervandalism.net>
2015-07-25 18:26:12 +0300

    No props for you
    Change-Id: Ic463c28ba0b4c2f744340d5eea4222b9e9220ddd

--- DPLforum_body.php
+++ DPLforum_body.php
@@ -499,6 +499,7 @@ class DPLForum {
 			$text = substr( $text, strlen( $this->sOmit ) );
 		}
 
+		$props = '';
 		if ( is_numeric( $time ) ) {
 			if ( $this->bTimestamp ) {
 				$query = 't=' . $time;
