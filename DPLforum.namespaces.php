771aa864169881ad016df6d4b4429060166d9589
Jack Phoenix <ashley@uncyclomedia.co>
2021-04-03 21:52:00 +0300

    Add missing namespace aliases
    Add namespace aliases for the following languages: ES, FA, JA, KO, NO, PL, RU, SR-EC, SR-EL, VI.
    
    Aliases come from the old Fandom/Wikia codebase and are available at the URL:
    https://github.com/Wikia/app/blob/2a5fb31decc6aa5278cc04d93236b0aa8e424dd3/includes/wikia/Extensions.php#L22
    
    Bug: T266324
    Change-Id: If51e70bf852b9d191b9ef44aef349fb181439e41
    (cherry picked from commit 591c364d7b0c8ee1712cfdc880fe606c27ba3dd6)

--- DPLforum.namespaces.php
+++ DPLforum.namespaces.php
@@ -5,25 +5,85 @@
  * @file
  */
 
-$namespaceNames = array();
+$namespaceNames = [];
 
 // For wikis where the DPLforum extension is not installed.
-if( !defined( 'NS_FORUM' ) ) {
+if ( !defined( 'NS_FORUM' ) ) {
 	define( 'NS_FORUM', 110 );
 }
 
-if( !defined( 'NS_FORUM_TALK' ) ) {
+if ( !defined( 'NS_FORUM_TALK' ) ) {
 	define( 'NS_FORUM_TALK', 111 );
 }
 
 /** English */
-$namespaceNames['en'] = array(
+$namespaceNames['en'] = [
 	NS_FORUM => 'Forum',
 	NS_FORUM_TALK => 'Forum_talk',
-);
+];
+
+/** Spanish (español) */
+$namespaceNames['es'] = [
+	NS_FORUM => 'Foro',
+	NS_FORUM_TALK => 'Foro_Discusión',
+];
+
+/** Persian (فارسی) */
+$namespaceNames['fa'] = [
+	NS_FORUM => 'فوروم',
+	NS_FORUM_TALK => 'بحث_فوروم',
+];
 
 /** Finnish (Suomi) */
-$namespaceNames['fi'] = array(
+$namespaceNames['fi'] = [
 	NS_FORUM => 'Foorumi',
 	NS_FORUM_TALK => 'Keskustelu_foorumista',
-);
+];
+
+/** Japanese (日本語) */
+$namespaceNames['ja'] = [
+	NS_FORUM => 'フォーラム',
+	NS_FORUM_TALK => 'フォーラム・トーク',
+];
+
+/** Korean (한국어) */
+$namespaceNames['ko'] = [
+	NS_FORUM => '포럼',
+	NS_FORUM_TALK => '포럼토론',
+];
+
+/** Norwegian (norsk) */
+$namespaceNames['no'] = [
+	NS_FORUM => 'Forum',
+	NS_FORUM_TALK => 'Forumdiskusjon',
+];
+
+/** Polish (polski) */
+$namespaceNames['pl'] = [
+	NS_FORUM => 'Forum',
+	NS_FORUM_TALK => 'Dyskusja_forum',
+];
+
+/** Russian (русский) */
+$namespaceNames['ru'] = [
+	NS_FORUM => 'Форум',
+	NS_FORUM_TALK => 'Обсуждение_форума',
+];
+
+/** Serbian Cyrillic (српски (ћирилица)) */
+$namespaceNames['sr-ec'] = [
+	NS_FORUM => 'Форум',
+	NS_FORUM_TALK => 'Разговор_о_форуму',
+];
+
+/** Serbian Latin (srpski (latinica)) */
+$namespaceNames['sr-el'] = [
+	NS_FORUM => 'Forum',
+	NS_FORUM_TALK => 'Razgovor_o_forumu',
+];
+
+/** Vietnamese (Tiếng Việt) */
+$namespaceNames['vi'] = [
+	NS_FORUM => 'Diễn_đàn',
+	NS_FORUM_TALK => 'Thảo_luận_Diễn_đàn',
+];
